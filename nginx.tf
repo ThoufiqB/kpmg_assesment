fe82bcad838a5ecaabab7b18c4fd6be59954ac7c
thoufiqb <thoufiqb2023@gmail.com>
2024-02-03 18:26:28 +0000

    kubeconfig cert update

--- nginx.tf
+++ nginx.tf
@@ -2,9 +2,9 @@ provider "helm" {
   kubernetes {
     host     = data.aws_eks_cluster.cluster.endpoint
     token    = data.aws_eks_cluster_auth.cluster.token
-    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    # client_certificate     = data.aws_eks_cluster_auth.cluster.client_certificate
-    # client_key             = data.aws_eks_cluster_auth.cluster.client_key
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    client_certificate     = data.aws_eks_cluster_auth.cluster.client_certificate
+    client_key             = data.aws_eks_cluster_auth.cluster.client_key
     # client_certificate     = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     # client_key             = base64decode(data.aws_eks_cluster_auth.cluster.client_certificate)
     # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
